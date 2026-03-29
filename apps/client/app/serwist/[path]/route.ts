d266799d54dd6bcf175385fcea7adbc284237114
Sydney <68250402+cecelot@users.noreply.github.com>
2026-03-29 10:43:48 -0400

    fix: precache routes

--- apps/client/app/serwist/[path]/route.ts
+++ apps/client/app/serwist/[path]/route.ts
@@ -1,8 +1,18 @@
+import { spawnSync } from "node:child_process";
 import { createSerwistRoute } from "@serwist/turbopack";
+
+const revision =
+  spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ??
+  crypto.randomUUID();
 
 export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
   createSerwistRoute({
-    additionalPrecacheEntries: [],
+    additionalPrecacheEntries: [
+      { url: "/", revision },
+      { url: "/nfa", revision },
+      { url: "/tm", revision },
+      { url: "/~offline", revision },
+    ],
     swSrc: "app/sw.ts",
     useNativeEsbuild: true,
   });
